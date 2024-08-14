3a0af5182c16c190947a26940d97defab4af30d6
umut kavakli <umutkavakli.ukk@gmail.com>
2024-08-14 16:59:56 +0300

    summarizers are gathered in one general file to keep Don't Repeat Yourself

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import gradio as gr
-import youtube_summarizer
-import pdf_summarizer 
-import webpage_summarizer
+from summarizer import get_summarization
 
 models = ["Llama 2"]
-sources = ["Youtube Video", "PDF", "Webpage URL"]
+sources = {
+    "Youtube Video URL": "youtube", 
+     "PDF": "pdf", 
+    "Webpage URL": "webpage"
+}
+
 
 css = """
 h1 {
@@ -19,13 +22,13 @@ def update_source(source):
     web_visibility = (source == "Webpage URL")
     return gr.Textbox(visible=youtube_visibility), gr.UploadButton(visible=pdf_visibility), gr.Textbox(visible=web_visibility)
 
-def out(youtube, pdf, web, source):
-    if source == "Youtube Video":
-        return youtube_summarizer.get_summarization(youtube)
-    elif source == "PDF":
-        return pdf_summarizer.get_summarization(pdf)
+def result(youtube, pdf, web, source_type):
+    if source_type == "Youtube Video":
+        return get_summarization(youtube, sources[source_type])
+    elif source_type == "PDF":
+        return get_summarization(pdf, sources[source_type])
     else:
-        return webpage_summarizer.get_summarization(web)
+        return get_summarization(web, sources[source_type])
 
 with gr.Blocks(css=css) as demo:
     gr.Markdown(
@@ -39,15 +42,16 @@ with gr.Blocks(css=css) as demo:
     with gr.Row():
         with gr.Column():
             model = gr.Dropdown(choices=models, label="Model", value=models[0], interactive=True)
-            task = gr.Dropdown(choices=sources, label="Source", value=sources[0], interactive=True)
+            task = gr.Dropdown(choices=list(sources.keys()), label="Source", value=list(sources.keys())[0], interactive=True)
         with gr.Column():
-            youtube = gr.Textbox(label="Youtube URL", visible=True)
-            pdf = gr.UploadButton("Click to Upload a PDF File", file_types=["file"], file_count="single", visible=False)
-            web = gr.Textbox(label="Webpage URL", visible=False)
-            task.change(update_source, inputs=[task], outputs=[youtube, pdf, web])
+            youtube_box = gr.Textbox(label="Youtube URL", visible=True)
+            pdf_box = gr.UploadButton("Click to Upload a PDF File", file_types=["file"], file_count="single", visible=False)
+            webpage_box = gr.Textbox(label="Webpage URL", visible=False)
+            task.change(update_source, inputs=[task], outputs=[youtube_box, pdf_box, webpage_box])
+
             output = gr.Textbox(label="Summarization", lines=10)
             summarize_button = gr.Button("Summarize")
-            summarize_button.click(fn=out, inputs=[youtube, pdf, web, task], outputs=[output])
+            summarize_button.click(fn=result, inputs=[youtube_box, pdf_box, webpage_box, task], outputs=[output])
 
 # Launch the Gradio interface
 demo.launch()
